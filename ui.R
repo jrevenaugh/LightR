21c645852d096d14d8fc507cb9cf8d636ce6fcba
Justin Revenaugh <justinr@umn.edu>
2018-05-07 19:09:40 -0500

    Added title.

--- ui.R
+++ ui.R
@@ -7,6 +7,7 @@
 require(shiny)
 
 ui <- bootstrapPage(
+  title = "Lights Out",
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   plotOutput(outputId = "board",
              height = "100%",
